ccbaf1203658663d011c5abcb73ca769d21ff4dd
jockey10 <shane.boulden@gmail.com>
2018-09-21 16:31:54 +1000

    use latest spring boot and micrometer metrics

--- src/main/java/com/rock/labs/cats/CatMetricsApplication.java
+++ src/main/java/com/rock/labs/cats/CatMetricsApplication.java
@@ -1,36 +1,19 @@
 package com.rock.labs.cats;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import io.prometheus.client.exporter.MetricsServlet;
-import io.prometheus.client.hotspot.DefaultExports;
-import io.prometheus.client.spring.boot.SpringBootMetricsCollector;
-import org.springframework.boot.actuate.endpoint.PublicMetrics;
-
-import java.util.Collection;
-
-@SpringBootApplication
+@SpringBootApplication()
 public class CatMetricsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CatMetricsApplication.class, args);
     }
 
     @Bean
-    SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
-
-        SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(publicMetrics);
-        springBootMetricsCollector.register();
-
-        return springBootMetricsCollector;
-    }
-
-    @Bean
-    ServletRegistrationBean servletRegistrationBean() {
-        DefaultExports.initialize();
-        return new ServletRegistrationBean(new MetricsServlet(), "/metrics");
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "catmetrics");
     }
 }
